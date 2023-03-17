bbfc01e2f11316222540bc2f262c0d199505764c
asdfadfzcv <yunjun556@gmail.com>
2023-03-17 09:41:23 +0900

    aa

--- js/common.js
+++ js/common.js
@@ -11,23 +11,7 @@ $(function () {
     $(".search_box").removeClass("on")
   })
 
-  $(".nav_list:nth-child(1) ").on("click", function () {
-    $(".nav_list:nth-child(1) ul").addClass("on").stop().slideToggle(400)
-  })
-
-  $(".nav_list:nth-child(2) ").on("click", function () {
-    $(".nav_list:nth-child(2) ul").addClass("on").stop().slideToggle(400)
-  })
-
-  $(".nav_list:nth-child(3) ").on("click", function () {
-    $(".nav_list:nth-child(3) ul").addClass("on").stop().slideToggle(400)
-  })
-
-  $(".nav_list:nth-child(4) ").on("click", function () {
-    $(".nav_list:nth-child(4) ul").addClass("on").stop().slideToggle(400)
-  })
-
-  $(".nav_list:nth-child(5) ").on("click", function () {
-    $(".nav_list:nth-child(5) ul").addClass("on").stop().slideToggle(400)
+  $(".nav_list").click(function () {
+    $(this).children("ul").toggleClass("on").stop().slideToggle(400)
   })
 })
